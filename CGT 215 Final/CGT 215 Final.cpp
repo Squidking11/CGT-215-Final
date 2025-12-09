576e48a8faa4d59e3339b98c41cb790ec4b1316e
Squidking11 <nicobutler4854@gmail.com>
2025-12-09 15:22:04 -0500

    Added spaceship and the ability to fly around. This took way longer than it should :|

--- CGT 215 Final/CGT 215 Final.cpp
+++ CGT 215 Final/CGT 215 Final.cpp
@@ -4,7 +4,12 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 #include "SFPhysics.h"
+#include <math.h>
 #include <Windows.h>
+#include <numbers>
+
+constexpr auto PI = (3.141592653589793238462643383279502884);
+
 using namespace std;
 using namespace sf;
 using namespace sfp;
@@ -14,12 +19,27 @@ void bounce(PhysicsBodyCollisionResult result)
 	cout << "Bounce!" << endl;
 }
 
-void DoInput(PhysicsSprite& ship) {
+void DoInput(PhysicsCircle& ship) {
 	if (Keyboard::isKeyPressed(Keyboard::Right)) {
-		ship.applyImpulse(Vector2f(.0005, 0));
+
+		ship.rotate(.1);
+		//ship.applyImpulse(Vector2f(.0005, 0));
 	}
 	if (Keyboard::isKeyPressed(Keyboard::Left)) {
-		ship.applyImpulse(Vector2f(-.0005, 0));
+		ship.rotate(-.1);
+		//ship.applyImpulse(Vector2f(-.0005, 0));
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Up)) {
+		float angle = ship.getRotation();
+		float dirX(cosf((angle - 90) * (PI / 180)));
+		float dirY(sinf((angle - 90) * (PI / 180)));
+		ship.applyImpulse(Vector2f(dirX, dirY) * .0001f);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Down)) {
+		float angle = ship.getRotation();
+		float dirX(cosf((angle - 90) * (PI / 180)));
+		float dirY(sinf((angle - 90) * (PI / 180)));
+		ship.applyImpulse(-(Vector2f(dirX, dirY) * .0001f));
 	}
 	
 }
@@ -29,29 +49,37 @@ int main()
 	/*char cwd[MAX_PATH];
 	GetCurrentDirectoryA(MAX_PATH, cwd);
 	cout << "Working directory: " << cwd << endl;*/
-	RenderWindow window(VideoMode(800, 600), "CGT 215 Final Project");
-	World world(Vector2f(0, 1.0));
+	RenderWindow window(VideoMode(1000, 800), "CGT 215 Final Project");
+	World world(Vector2f(0, 0));
 	//PhysicsCircle ball;
-	PhysicsSprite ship;
+	//PhysicsSprite ship;
+	PhysicsCircle shipBox;
 	Texture tex;
 	if (!tex.loadFromFile("SpaceShip.png")) {
 		cout << "Failed to load texture" << endl;
 	}
-	ship.setTexture(tex);
-	ship.setCenter(Vector2f(400, 300));
-	ship.setStatic(true);
-	world.AddPhysicsBody(ship);
-	PhysicsRectangle floor;
-	floor.setSize(Vector2f(800, 10));
-	floor.setCenter(Vector2f(400, 590));
-	floor.setStatic(true);
-	int bounceCount(0);
-	floor.onCollision = [&bounceCount](PhysicsBodyCollisionResult result) {
-		bounceCount++;
-		cout << "Bounce " << bounceCount << endl;
-	};
+
+	Sprite ship(tex);
+	shipBox.setSize(Vector2f(50, 50));
+	shipBox.setOrigin(25, 25);
+	shipBox.setCenter(Vector2f(500, 400));
+	ship.setOrigin(Vector2f(tex.getSize().x / 2, tex.getSize().y / 2));
+	ship.setScale(Vector2f(.5, .5));
+	ship.setPosition(Vector2f(shipBox.getCenter().x + 50, shipBox.getCenter().y + 50));
 	
-	world.AddPhysicsBody(floor);
+
+	world.AddPhysicsBody(shipBox);
+
+	//ship.scale(Vector2f(0.1f, 0.1f));
+	//ship.setTexture(tex);
+	//ship.setOrigin(ship.getCenter());
+
+	//ship.setSize(Vector2f(100, 50));
+	//ship.setCenter(Vector2f(500, 400));
+	//ship.getBounds().setSize(Vector2f(50, 50));
+	
+	//world.AddPhysicsBody(ship);
+	
 	Clock clock;
 	Time lastTime = clock.getElapsedTime();
 	while (true) {
@@ -61,11 +89,13 @@ int main()
 		if (deltaTimeMS > 0) {
 			world.UpdatePhysics(deltaTimeMS);
 			lastTime = currentTime;
-			DoInput(ship);
+			DoInput(shipBox);
 		}
+		ship.setPosition(Vector2f(shipBox.getCenter().x + 50, shipBox.getCenter().y + 50));
+		ship.setRotation(shipBox.getRotation());
 		window.clear();
 		window.draw(ship);
-		window.draw(floor);
+		//window.draw(shipBox);
 		world.VisualizeAllBounds(window);
 		window.display();
 
