7b85f91441ac33bdbfcefc44f8ed31f70d2c5346
Squidking11 <nicobutler4854@gmail.com>
2025-12-09 19:49:32 -0500

    Added screen wrap and half of the stupid laser beams

--- CGT 215 Final/CGT 215 Final.cpp
+++ CGT 215 Final/CGT 215 Final.cpp
@@ -19,40 +19,60 @@ void bounce(PhysicsBodyCollisionResult result)
 	cout << "Bounce!" << endl;
 }
 
-void DoInput(PhysicsCircle& ship) {
-	if (Keyboard::isKeyPressed(Keyboard::Right)) {
+void DoInput(PhysicsCircle& ship, Texture *laserTex, PhysicsShapeList<PhysicsSprite> *lasers,
+			 bool *fireCoolDown, World *world) {
+	float angle = ship.getRotation();
+	float dirX(cosf((angle - 90) * (PI / 180)));
+	float dirY(sinf((angle - 90) * (PI / 180)));
+	if (Keyboard::isKeyPressed(Keyboard::D)) {
 
 		ship.rotate(.1);
 		//ship.applyImpulse(Vector2f(.0005, 0));
 	}
-	if (Keyboard::isKeyPressed(Keyboard::Left)) {
+	if (Keyboard::isKeyPressed(Keyboard::A)) {
 		ship.rotate(-.1);
 		//ship.applyImpulse(Vector2f(-.0005, 0));
 	}
-	if (Keyboard::isKeyPressed(Keyboard::Up)) {
-		float angle = ship.getRotation();
-		float dirX(cosf((angle - 90) * (PI / 180)));
-		float dirY(sinf((angle - 90) * (PI / 180)));
+	if (Keyboard::isKeyPressed(Keyboard::W)) {
 		ship.applyImpulse(Vector2f(dirX, dirY) * .0001f);
 	}
-	if (Keyboard::isKeyPressed(Keyboard::Down)) {
-		float angle = ship.getRotation();
-		float dirX(cosf((angle - 90) * (PI / 180)));
-		float dirY(sinf((angle - 90) * (PI / 180)));
+	if (Keyboard::isKeyPressed(Keyboard::S)) {
 		ship.applyImpulse(-(Vector2f(dirX, dirY) * .0001f));
 	}
-	
+	if (*fireCoolDown && Keyboard::isKeyPressed(Keyboard::Space)) {
+		*fireCoolDown = false;
+		PhysicsSprite& laser = lasers->Create();
+		laser.setTexture(*laserTex);
+		Vector2f sz = laser.getSize();
+		//laser.setSize(Vector2f(20, 5));
+		laser.setScale(.5, .5);
+		laser.setRotation(ship.getRotation());
+
+		laser.getBounds().setSize(Vector2f(20, 5));
+		laser.setCenter(Vector2f(ship.getCenter().x + 50, ship.getCenter().y + 50));
+		world->AddPhysicsBody(laser);
+	}
+}
+
+void wrapScreen(PhysicsCircle *obj) {
+	if (obj->getCenter().x < -50) {
+		obj->setCenter(Vector2f(1000, obj->getCenter().y));
+	}
+	else if (obj->getCenter().x > 1050) {
+		obj->setCenter(Vector2f(0, obj->getCenter().y));
+	}
+	if (obj->getCenter().y < -50) {
+		obj->setCenter(Vector2f(obj->getCenter().x, 800));
+	}
+	else if (obj->getCenter().y > 850) {
+		obj->setCenter(Vector2f(obj->getCenter().x, 0));
+	}
 }
 
 int main()
 {
-	/*char cwd[MAX_PATH];
-	GetCurrentDirectoryA(MAX_PATH, cwd);
-	cout << "Working directory: " << cwd << endl;*/
 	RenderWindow window(VideoMode(1000, 800), "CGT 215 Final Project");
 	World world(Vector2f(0, 0));
-	//PhysicsCircle ball;
-	//PhysicsSprite ship;
 	PhysicsCircle shipBox;
 	Texture tex;
 	if (!tex.loadFromFile("SpaceShip.png")) {
@@ -61,54 +81,48 @@ int main()
 
 	Sprite ship(tex);
 	shipBox.setSize(Vector2f(50, 50));
-	shipBox.setOrigin(25, 25);
+	//shipBox.setOrigin(25, 25);
 	shipBox.setCenter(Vector2f(500, 400));
 	ship.setOrigin(Vector2f(tex.getSize().x / 2, tex.getSize().y / 2));
 	ship.setScale(Vector2f(.5, .5));
 	ship.setPosition(Vector2f(shipBox.getCenter().x + 50, shipBox.getCenter().y + 50));
 	
-
 	world.AddPhysicsBody(shipBox);
 
-	//ship.scale(Vector2f(0.1f, 0.1f));
-	//ship.setTexture(tex);
-	//ship.setOrigin(ship.getCenter());
+	Texture laserTex;
+	if (!laserTex.loadFromFile("LaserBeam.png")) {
+		cout << "Failed to load laser texture" << endl;
+	}
+	PhysicsShapeList<PhysicsSprite> lasers;
 
-	//ship.setSize(Vector2f(100, 50));
-	//ship.setCenter(Vector2f(500, 400));
-	//ship.getBounds().setSize(Vector2f(50, 50));
-	
-	//world.AddPhysicsBody(ship);
-	
 	Clock clock;
 	Time lastTime = clock.getElapsedTime();
+	Time lastFireTime = clock.getElapsedTime();
+	bool fireCoolDown = true;
 	while (true) {
 		Time currentTime(clock.getElapsedTime());
 		Time deltaTime = currentTime - lastTime;
 		int deltaTimeMS(deltaTime.asMilliseconds());
+		int fireDeltaTime = (currentTime - lastFireTime).asMilliseconds();
 		if (deltaTimeMS > 0) {
 			world.UpdatePhysics(deltaTimeMS);
 			lastTime = currentTime;
-			DoInput(shipBox);
+			DoInput(shipBox, &laserTex, &lasers, &fireCoolDown, &world);
+			wrapScreen(&shipBox);
+		}
+		if (fireDeltaTime >= 200) {
+			lastFireTime = currentTime;
+			fireCoolDown = true;
 		}
 		ship.setPosition(Vector2f(shipBox.getCenter().x + 50, shipBox.getCenter().y + 50));
 		ship.setRotation(shipBox.getRotation());
+
 		window.clear();
+		for (auto laser : lasers) {
+			window.draw(laser);
+		}
 		window.draw(ship);
-		//window.draw(shipBox);
 		world.VisualizeAllBounds(window);
 		window.display();
-
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
